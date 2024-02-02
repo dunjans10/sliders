4ca3a99a7de35fffbed1511d2377702f95001ec1
Dunja <dunjabeki@gmail.com>
2024-02-02 10:11:49 +0100

    adding third slider

--- script.js
+++ script.js
@@ -72,6 +72,17 @@ function showPagination() {
 
 window.addEventListener('resize', adjustSlidesPerView);
 
+//Marquee
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+} 
 
 //Slider two
 
@@ -152,3 +163,8 @@ function updateButtonsVisibility() {
 }
 
 
+/* Third slider*/ 
+
+function changeImage(imageSrc) {
+  document.getElementById('main-image').src = imageSrc;
+}
